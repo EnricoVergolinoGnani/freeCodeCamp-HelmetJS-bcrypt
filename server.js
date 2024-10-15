27f9e3e53aaaa341ef8ba70244b11654fd456103
enricov <enricov@tecgraf.puc-rio.br>
2024-10-15 16:15:31 -0300

    Hashing and Comparing Passwords Asynchronously

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    /*Store hash in your db*/
+    console.log(hash);  //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        /*res == true or false*/
+        console.log(res)
+    });
+});
 
 //END_ASYNC
 
